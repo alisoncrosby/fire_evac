a4f700efc12361fc4376969881d091093f51b307
moarKitties <99438834+moarKitties@users.noreply.github.com>
2024-06-27 17:14:10 -0700

    added screen fade from tutorial to main

--- Assets/cubeStart.cs
+++ Assets/cubeStart.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class cubeStart : MonoBehaviour
 {
+
+    private float timer = -1;
+    public Image dark;
+
+    private void Update()
+    {
+        if(timer >= 0)
+        {
+            timer += Time.deltaTime;
+            dark.color = Color.Lerp(dark.color, Color.black,timer/3f);
+            if(timer >=3)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+
         if(other.tag == "StartBox")
         {
-            SceneManager.LoadScene(1);
+            timer = 0;
         }
     }
 }
