27ed14b218b18f6c1197101a46ead2b043b424cf
mjohns2355 <mljohns@ucsc.edu>
2024-12-24 10:02:58 -0800

    scene change for teleport box

--- Assets/teleportBox.cs
+++ Assets/teleportBox.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class teleportBox : MonoBehaviour
 {
 
-    public Transform teleportSpot;
+    private Transform teleportSpot;
     public Animator packed1;
     public Animator packed2;
+
+    private void Start()
+    {
+        teleportSpot = GameObject.FindGameObjectWithTag("TeleportSpot").transform;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
